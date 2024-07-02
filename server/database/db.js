c3a7b313d31159fef05ded0964667f8a89a17898
amir-kedis <amir.kedis@gmail.com>
2024-07-02 10:32:33 +0300

    feat(docker): db dockerization and simple fill script

--- server/database/db.js
+++ server/database/db.js
@@ -1,29 +1,42 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
-import newWeekScheduler from './scheduler.js'
+import pg from "pg";
+import dotenv from "dotenv";
+import newWeekScheduler from "./scheduler.js";
 
-dotenv.config()
+dotenv.config();
 
-let _db
-if (process.env.DB === 'online') {
-    _db = new pg.Pool({
-        connectionString: process.env.DB_URI,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    })
+console.log("==== DB CONFIG ====");
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  URL: process.env.DATABASE_URL,
+});
+
+let _db;
+if (process.env.DB === "online") {
+  console.log("trying to connect to online db");
+  _db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 } else {
-    _db = new pg.Pool({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DATABASE,
-    })
+  console.log("trying to connect to local db");
+  _db = new pg.Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    URL: process.env.DATABASE_URL,
+  });
 }
 
-const db = _db
+const db = _db;
 
-newWeekScheduler.start()
+newWeekScheduler.start();
 
-export default db
+export default db;
