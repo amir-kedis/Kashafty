c7b656f8afbdea343ee82737e3c6f1ff0222bdef
amir-kedis <amir.kedis@gmail.com>
2024-07-05 16:07:21 +0300

    chore(prisma): clean old db driver now fully using prisma

--- server/database/db.ts
+++ server/database/db.ts
@@ -1,4 +1,3 @@
-import pg, { Pool } from "pg";
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 import newWeekScheduler from "./scheduler";
@@ -7,38 +6,11 @@ dotenv.config();
 
 console.log("==== DB CONFIG ====");
 console.log({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
   URL: process.env.DATABASE_URL,
 });
 
-let db: Pool;
-
 const prisma = new PrismaClient();
-
-if (process.env.DB === "online") {
-  console.log("trying to connect to online db");
-  db = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  console.log("trying to connect to local db");
-  db = new pg.Pool({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "5432", 10),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-  });
-}
 
 newWeekScheduler.start();
 
 export { prisma };
-export default db;
