ea15dad907c899f66f7ef21d2d6c12f4cd8b748b
amir-kedis <amir.kedis@gmail.com>
2024-07-14 20:52:04 +0300

    feat: add cron job for sending attendance wraning for captains

--- server/routes/cron.route.ts
+++ server/routes/cron.route.ts
@@ -4,9 +4,15 @@ import cronController from "../controllers/cron.controller";
 const cronRouter = Router();
 
 cronRouter.post("/week", cronController.createMissingWeeks);
+
 cronRouter.post(
   "/attendance/scout",
   cronController.sendScoutsAbsenceNotification,
+);
+
+cronRouter.post(
+  "/attendance/captain",
+  cronController.sendCaptainsAbsenceNotification,
 );
 
 export default cronRouter;
