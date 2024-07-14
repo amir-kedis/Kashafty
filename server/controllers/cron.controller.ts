3b1ff49628af6cda936df18eedb2de3ac142c984
amir-kedis <amir.kedis@gmail.com>
2024-07-14 20:52:04 +0300

    feat: add cron job for sending attendance wraning for captains

--- server/controllers/cron.controller.ts
+++ server/controllers/cron.controller.ts
@@ -189,6 +189,102 @@ const cronController = {
       res.status(500).json({ message: "Internal server error" });
     }
   },
+
+  sendCaptainsAbsenceNotification: async (req: Request, res: Response) => {
+    try {
+      const curTerm = await prisma.term.findFirst({
+        orderBy: {
+          termNumber: "desc",
+        },
+        include: {
+          Week: {
+            include: {
+              ScoutAttendance: true,
+            },
+          },
+        },
+      });
+
+      if (!curTerm) {
+        return res.status(404).json({ message: "No current term found" });
+      }
+
+      const today = new Date();
+      const startDate = new Date(curTerm.startDate);
+      const totalWeeks = Math.ceil(
+        (today.getTime() - startDate.getTime()) / (7 * 24 * 60 * 60 * 1000),
+      );
+
+      const attendanceRates = await prisma.captain.findMany({
+        include: {
+          CaptainAttendance: {
+            where: {
+              Week: {
+                termNumber: curTerm.termNumber,
+              },
+            },
+          },
+        },
+      });
+
+      const captainsToNotify = attendanceRates.filter((scout) => {
+        const totalDays = scout.CaptainAttendance.length;
+        const totalAttendance = scout.CaptainAttendance.filter(
+          (attendance) => attendance.attendanceStatus === "attended",
+        ).length;
+        const attendanceRate = totalAttendance / totalDays;
+
+        return attendanceRate < 0.5;
+      });
+
+      const generalCaptains = await prisma.captain.findMany({
+        where: {
+          type: "general",
+        },
+      });
+
+      for (const captain of captainsToNotify) {
+        const unitCaptinOfSector = await prisma.sector.findFirst({
+          where: {
+            baseName: captain.rSectorBaseName as string,
+            suffixName: captain.rSectorSuffixName as string,
+          },
+        });
+
+        const notifications = [];
+
+        generalCaptains.forEach((captain) => {
+          notifications.push({
+            captainId: captain.captainId,
+            title: "غياب كشاف",
+            message: `الكشاف ${captain.firstName} ${captain.middleName} تخطى غيابه الـ 50%`,
+            type: NotificationType.attendance,
+            status: NotificationStatus.UNREAD,
+          });
+        });
+
+        if (unitCaptinOfSector?.unitCaptainId)
+          notifications.push({
+            captainId: unitCaptinOfSector?.unitCaptainId,
+            title: "غياب كشاف",
+            message: `القائد/ة ${captain.firstName} ${captain.middleName} تخطى غيابه الـ 50%`,
+            type: NotificationType.attendance,
+            status: NotificationStatus.UNREAD,
+          });
+
+        await prisma.notification.createMany({
+          data: notifications,
+        });
+      }
+
+      res
+        .status(200)
+        .json({ message: "send notifications for " + captainsToNotify.length });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
 };
 
 export default cronController;
