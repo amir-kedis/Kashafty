8ef44cf97f178687a24848d5424411a94b64ebc9
amir-kedis <amir.kedis@gmail.com>
2024-07-27 13:33:02 +0300

    fix: disable strict mode in production

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const isStrict = import.meta.env.VITE_REACT_STRICT_MODE as string;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    {isStrict == "true" ? (
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    ) : (
+      <App />
+    )}
+  </>,
 );
