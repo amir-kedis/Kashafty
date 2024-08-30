31bad0e9774cd6aea1da984f7d88e75e4e56d459
shehab299 <shehab299@outlook.com>
2024-08-30 17:38:19 +0300

    Refactored Activities - auth - captain

--- server/controllers/activities.controller.ts
+++ server/controllers/activities.controller.ts
@@ -1,58 +1,50 @@
 import { Request, Response } from "express";
 import { prisma } from "../database/db";
+import AppError from "../utils/AppError";
+import asyncDec from "../utils/asyncDec";
+
+
+
+async function insertActivity(req: Request, res: Response){
+
+  const { name, place, weekNumber, termNumber, day, type } = req.body;
+
+  const activity = await prisma.activity.create({
+    data: {
+      name,
+      place,
+      weekNumber: parseInt(weekNumber),
+      termNumber: parseInt(termNumber),
+      day,
+      type,
+    },
+  });
+
+  if (!activity) {
+    throw new AppError(400, "Insertion failed", "فشلت عملية الإدخال");
+  }
+
+  return res.status(200).json({
+    message: "Successful insertion",
+    body: activity,
+  });
+
+}
+
+async function getAllActivities(req: Request, res: Response) {
+
+  const activities = await prisma.activity.findMany();
+
+  return res.status(200).json({
+    message: "Successful retrieval",
+    body: activities,
+    count: activities.length,
+  });
+}
 
 const activitiesController = {
-  insertActivity: async (req: Request, res: Response): Promise<any> => {
-    try {
-      const { name, place, weekNumber, termNumber, day, type } = req.body;
-
-      const activity = await prisma.activity.create({
-        data: {
-          name,
-          place,
-          weekNumber: parseInt(weekNumber),
-          termNumber: parseInt(termNumber),
-          day,
-          type,
-        },
-      });
-
-      if (!activity) {
-        return res.status(400).json({
-          error: "Insertion failed",
-        });
-      }
-
-      return res.status(200).json({
-        message: "Successful insertion",
-        body: activity,
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        message: "An error occurred while inserting a new activity",
-        body: error,
-      });
-    }
-  },
-
-  getAllActivities: async (_: Request, res: Response): Promise<any> => {
-    try {
-      const activities = await prisma.activity.findMany();
-
-      return res.status(200).json({
-        message: "Successful retrieval",
-        body: activities,
-        count: activities.length,
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        message: "An error occurred while retrieving activities",
-        body: error,
-      });
-    }
-  },
+  insertActivity: asyncDec(insertActivity),
+  getAllActivities: asyncDec(getAllActivities)
 };
 
 export default activitiesController;
--- server/controllers/captain.controller.ts
+++ server/controllers/captain.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { prisma } from "../database/db";
 import { Captain } from "@prisma/client";
+import asyncDec from "../utils/asyncDec";
+import AppError from "../utils/AppError";
 
 interface GetAllCaptainsRequest extends Request {
   query: {
@@ -27,180 +29,142 @@ interface GetCaptainRequest extends Request {
   };
 }
 
+async function getAllCaptains(req: GetAllCaptainsRequest, res: Response) {
+
+  const { type } = req.query;
+
+  let result: Captain[] = await prisma.captain.findMany();
+
+
+  //TODO: Performance issue, consider using prisma query to filter the data
+  if (type) {
+    result = result.filter((captain) => captain.type === type);
+  }
+
+  return res.status(200).json({
+    message: "Successful retrieval",
+    body: result,
+    count: result.length,
+  });
+}
+
+async function getCaptainsInSector(req: GetCaptainsInSectorRequest, res: Response) {
+  const { baseName, suffixName } = req.query;
+
+  const result = await prisma.captain.findMany({
+    where: {
+      rSectorBaseName: baseName,
+      rSectorSuffixName: suffixName,
+    },
+  });
+
+  return res.status(200).json({
+    message: "Successful retrieval",
+    body: result,
+    count: result.length,
+  });
+}
+
+async function getCaptainsInUnit(req: GetCaptainsInUnitRequest, res: Response) {
+  
+  const { unitCaptainId } = req.params;
+
+  // NOTE: left as a ref remove after the testing the below todo
+  // const result = await db.query(
+  //   `SELECT C.*
+  //    FROM "Captain" AS C, "Sector" AS S
+  //    WHERE S."unitCaptainId" = $1 AND
+  //          C."rSectorBaseName" = S."baseName" AND
+  //          C."rSectorSuffixName" = S."suffixName";`,
+  //   [unitCaptainId],
+  // );
+
+  // TODO: this needs testing
+  console.log(unitCaptainId);
+
+  // TODO: what if UnitCaptainId is not found 
+
+  const result = await prisma.captain.findMany({
+    where: {
+      Sector_Sector_unitCaptainIdToCaptain: {
+        some: {
+          unitCaptainId: parseInt(unitCaptainId),
+        },
+      },
+    },
+    include: {
+      Sector_Sector_unitCaptainIdToCaptain: true,
+    },
+  });
+
+  res.status(200).json({
+    message: "Successful retrieval",
+    body: result,
+    count: result.length,
+  });
+
+}
+
+async function getCaptain(req: GetCaptainRequest, res: Response) {
+
+  const { captainId } = req.params;
+
+  const result = await prisma.captain.findUnique({
+    where: {
+      captainId: parseInt(captainId),
+    },
+  });
+
+  if (!result) {
+    throw new AppError(404, "Captain not found", "الكابتن غير موجود");
+  }
+
+  res.status(200).json({
+    message: "Successful retrieval",
+    body: result,
+  });
+}
+
+// @desc    Update a captain type
+// @route   PATCH /api/captain/change/type/:id
+// @access  Private
+
+async function setCaptainType(req: Request, res: Response){
+
+  const { captainId } = req.params;
+  const { type } = req.body;
+
+  if (!captainId) {
+    throw new AppError(400, "Please enter a valid id", "من فضلك أدخل رقم كابتن صحيح");
+  }
+
+  if (type !== "regular" && type !== "unit" && type !== "general") {
+    throw new AppError(400, "Please enter a valid captain type", "من فضلك أدخل نوع كابتن صحيح");
+  }
+
+  const result = await prisma.captain.update({
+    where: {
+      captainId: parseInt(captainId),
+    },
+    data: {
+      type,
+    },
+  });
+
+  res.status(200).json({
+    message: "Successful update",
+    body: result,
+  });
+
+
+}
+
 const captainController = {
-  getAllCaptains: async (
-    req: GetAllCaptainsRequest,
-    res: Response,
-  ): Promise<void> => {
-    try {
-      const { type } = req.query;
-
-      let result: Captain[] = await prisma.captain.findMany();
-
-      if (type) {
-        result = result.filter((captain) => captain.type === type);
-      }
-
-      res.status(200).json({
-        message: "Successful retrieval",
-        body: result,
-        count: result.length,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        error: "An error occurred while retrieving the captains info",
-        body: error,
-      });
-    }
-  },
-
-  getCaptainsInSector: async (
-    req: GetCaptainsInSectorRequest,
-    res: Response,
-  ): Promise<void> => {
-    try {
-      const { baseName, suffixName } = req.query;
-
-      const result = await prisma.captain.findMany({
-        where: {
-          rSectorBaseName: baseName,
-          rSectorSuffixName: suffixName,
-        },
-      });
-
-      res.status(200).json({
-        message: "Successful retrieval",
-        body: result,
-        count: result.length,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: "An error occurred while retrieving data",
-        body: error,
-      });
-    }
-  },
-
-  getCaptainsInUnit: async (
-    req: GetCaptainsInUnitRequest,
-    res: Response,
-  ): Promise<void> => {
-    try {
-      const { unitCaptainId } = req.params;
-
-      // NOTE: left as a ref remove after the testing the below todo
-      // const result = await db.query(
-      //   `SELECT C.*
-      //    FROM "Captain" AS C, "Sector" AS S
-      //    WHERE S."unitCaptainId" = $1 AND
-      //          C."rSectorBaseName" = S."baseName" AND
-      //          C."rSectorSuffixName" = S."suffixName";`,
-      //   [unitCaptainId],
-      // );
-
-      // TODO: this needs testing
-      console.log(unitCaptainId);
-      const result = await prisma.captain.findMany({
-        where: {
-          Sector_Sector_unitCaptainIdToCaptain: {
-            some: {
-              unitCaptainId: parseInt(unitCaptainId),
-            },
-          },
-        },
-        include: {
-          Sector_Sector_unitCaptainIdToCaptain: true,
-        },
-      });
-
-      res.status(200).json({
-        message: "Successful retrieval",
-        body: result,
-        count: result.length,
-      });
-
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: "An error occurred while retrieving data",
-        body: error,
-      });
-    }
-  },
-
-  getCaptain: async (req: GetCaptainRequest, res: Response): Promise<any> => {
-    try {
-      const { captainId } = req.params;
-
-      const result = await prisma.captain.findUnique({
-        where: {
-          captainId: parseInt(captainId),
-        },
-      });
-
-      if (!result) {
-        return res.status(404).json({
-          error: "Captain not found!",
-        });
-      }
-
-      res.status(200).json({
-        message: "Successful retrieval",
-        body: result,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: "An error occurred while retrieving data",
-        body: error,
-      });
-    }
-  },
-
-  // @desc    Update a captain type
-  // @route   PATCH /api/captain/change/type/:id
-  // @access  Private
-  setCaptainType: async (req: Request, res: Response): Promise<any> => {
-    try {
-      const { captainId } = req.params;
-      const { type } = req.body;
-
-      if (!captainId) {
-        return res.status(400).json({
-          error: "Please enter a valid id",
-        });
-      }
-
-      if (type !== "regular" && type !== "unit" && type !== "general") {
-        return res.status(400).json({
-          error: "Please enter a valid captain type",
-        });
-      }
-
-      const result = await prisma.captain.update({
-        where: {
-          captainId: parseInt(captainId),
-        },
-        data: {
-          type,
-        },
-      });
-
-      res.status(200).json({
-        message: "Successful update",
-        body: result,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: "An error occurred while updating data",
-        body: error,
-      });
-    }
-  },
+  getAllCaptains: asyncDec(getAllCaptains),
+  getCaptainsInSector: asyncDec(getCaptainsInSector),
+  getCaptainsInUnit: asyncDec(getCaptainsInUnit),
+  getCaptain: asyncDec(getCaptain),
+  setCaptainType: asyncDec(setCaptainType),
 };
 
 export default captainController;
