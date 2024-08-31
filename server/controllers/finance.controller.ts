f201cb88cccb020f396ed6cfdf50808eff7444e3
shehab299 <shehab299@outlook.com>
2024-08-31 16:37:47 +0300

    refactor finance - term

--- server/controllers/finance.controller.ts
+++ server/controllers/finance.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { prisma } from "../database/db";
 import { Notification } from "@prisma/client";
+import asyncDec from "../utils/asyncDec";
 
 interface GetSubscriptionRequest extends Request {
   query: {
@@ -11,316 +12,305 @@ interface GetSubscriptionRequest extends Request {
   };
 }
 
+
+// @desc    Get a budget
+// @route   GET /api/finance/budget
+// @access  Private
+
+async function getBudget(_: Request, res: Response) {
+
+  const incomeAgg = await prisma.financeItem.aggregate({
+    _sum: {
+      value: true,
+    },
+    where: {
+      type: "income",
+    },
+  });
+
+  const income = incomeAgg._sum.value || 0;
+
+  const expenseAgg = await prisma.financeItem.aggregate({
+    _sum: {
+      value: true,
+    },
+    where: {
+      type: "expense",
+    },
+  });
+
+  const expense = expenseAgg._sum.value || 0;
+
+  const budget = income - expense;
+
+  return res.status(200).json({
+    message: "Get budget successfully",
+    body: budget,
+  });
+}
+
+// @desc    Get income
+// @route   GET /api/finance/income
+// @access  Private
+
+async function getIncome(_: Request, res: Response) {
+
+  const result = await prisma.financeItem.findMany({
+    where: {
+      type: "income",
+    },
+    include: {
+      OtherItem: true,
+    },
+  });
+
+  const income = result;
+
+  res.status(200).json({
+    message: "Get income successfully",
+    body: income,
+  });
+
+}
+
+
+// @desc    Get expense
+// @route   GET /api/finance/expense
+// @access  Private
+
+
+async function getExpense(_: Request, res: Response) {
+
+  const result = await prisma.financeItem.findMany({
+    where: {
+      type: "expense",
+    },
+    include: {
+      OtherItem: true,
+    },
+  });
+
+  const expense = result;
+
+  res.status(200).json({
+    message: "Get expense successfully",
+    body: expense,
+  });
+
+}
+
+
+
+// @desc    Add a subscription
+// @route   POST /api/finance/subscription
+// @access  Private
+
+async function addSubscription(req: Request, res: Response) {
+
+  const {
+    value,
+    sectorBaseName,
+    sectorSuffixName,
+    termNumber,
+    weekNumber,
+  } = req.body as {
+    value: number;
+    sectorBaseName: string;
+    sectorSuffixName: string;
+    termNumber: string;
+    weekNumber: string;
+  };
+
+  // let result = await db.query(
+  //   `SELECT "itemId" AS id
+  //           FROM "Subscription"
+  //           WHERE "sectorBaseName" = $1 AND "sectorSuffixName" = $2 AND "termNumber" = $3 AND "weekNumber" = $4;`,
+  //   [sectorBaseName, sectorSuffixName, termNumber, weekNumber],
+  // );
+
+  let subscr = await prisma.subscription.findFirst({
+    where: {
+      sectorBaseName: sectorBaseName,
+      sectorSuffixName: sectorSuffixName,
+      weekNumber: parseInt(weekNumber),
+      termNumber: parseInt(termNumber),
+    },
+  });
+
+  let result: any;
+
+  if (!subscr) {
+    result = await prisma.financeItem.create({
+      data: {
+        value: value,
+        type: "income",
+        timestamp: new Date(),
+      },
+    });
+  } else {
+    result = await prisma.financeItem.update({
+      where: {
+        itemId: subscr.itemId,
+      },
+      data: {
+        value: value,
+        timestamp: new Date(),
+      },
+    });
+  }
+
+  const financeItem = result;
+
+  result = await prisma.subscription.upsert({
+    where: {
+      itemId: financeItem.itemId,
+    },
+    update: {
+      sectorBaseName: sectorBaseName,
+      sectorSuffixName: sectorSuffixName,
+      weekNumber: parseInt(weekNumber),
+      termNumber: parseInt(termNumber),
+    },
+    create: {
+      itemId: financeItem.itemId,
+      sectorBaseName: sectorBaseName,
+      sectorSuffixName: sectorSuffixName,
+      weekNumber: parseInt(weekNumber),
+      termNumber: parseInt(termNumber),
+    },
+  });
+
+  const subscription = result;
+
+  // Notify general captains
+  const generalCaptains = await prisma.captain.findMany({
+    where: {
+      type: "general",
+    },
+  });
+
+  const notifications = generalCaptains.map((captain) => ({
+    captainId: captain.captainId,
+    type: "financeItemCreated",
+    status: "UNREAD",
+    title: "تحديث اشتراك",
+    message: `تم ${subscr ? "تحديث" : "إنشاء"} اشتراك لقطاع ${sectorBaseName} ${sectorSuffixName} بقيمة ${value}.`,
+  }));
+
+  const sentMsgs = await prisma.notification.createMany({
+    data: notifications as Notification[],
+  });
+
+  console.log(sentMsgs);
+
+  res.status(200).json({
+    message: "Add subscription successfully",
+    body: { financeItem: financeItem, subscription: subscription },
+  });
+}
+
+// @desc    Get all subscriptions of current week
+// @route   GET /api/finance/subscription/all
+// @access  Private
+
+async function getAllSubscriptionsOfCurrentWeek(req: any, res: Response){
+
+  const subscription = await prisma.subscription.findMany({
+    where: {
+      termNumber: req.currentWeek.termNumber,
+      weekNumber: req.currentWeek.weekNumber,
+    },
+    include: {
+      FinanceItem: true,
+    },
+  });
+
+  const subscriptionValue = subscription.reduce(
+    (acc, curr) => acc + curr.FinanceItem.value,
+    0,
+  );
+
+  res.status(200).json({
+    message: "Get subscription successfully",
+    body: subscriptionValue,
+  });
+}
+
+
+// @desc    Get a subscription
+// @route   GET /api/finance/subscription
+// @access  Private
+
+async function getSubscription(req: GetSubscriptionRequest, res: Response) {
+
+  let { sectorBaseName, sectorSuffixName, weekNumber, termNumber }: any =
+  req.query;
+
+  if (weekNumber === undefined)
+    weekNumber = req?.currentWeek?.weekNumber.toString();
+
+  if (termNumber === undefined)
+    termNumber = req?.currentWeek?.termNumber.toString();
+
+  const subscription = await prisma.subscription.findFirst({
+    where: {
+      sectorBaseName: sectorBaseName,
+      sectorSuffixName: sectorSuffixName,
+      weekNumber: parseInt(weekNumber),
+      termNumber: parseInt(termNumber),
+    },
+    include: {
+      FinanceItem: true,
+    },
+  });
+
+  res.status(200).json({
+    message: "Get subscription successfully",
+    body: subscription?.FinanceItem.value,
+  });
+
+}
+
+
+// @desc    Add an other item
+// @route   POST /api/finance/otherItem
+// @access  Private
+
+async function addOtherItem(req: Request, res: Response){
+
+  const { value, type, description } = req.body;
+
+  const financeItem = await prisma.financeItem.create({
+    data: {
+      value: parseInt(value),
+      type: type,
+      timestamp: new Date(),
+    },
+  });
+  const otherItem = await prisma.otherItem.create({
+    data: {
+      description: description,
+      itemId: financeItem.itemId,
+    },
+  });
+
+  res.status(200).json({
+    message: "Add other item successfully",
+    body: { financeItem: financeItem, otherItem: otherItem },
+  });
+
+}
+
+
 const financeController = {
-  // @desc    Get a budget
-  // @route   GET /api/finance/budget
-  // @access  Private
-  getBudget: async (_: Request, res: Response) => {
-    try {
-      // get income
-      const incomeAgg = await prisma.financeItem.aggregate({
-        _sum: {
-          value: true,
-        },
-        where: {
-          type: "income",
-        },
-      });
-      const income = incomeAgg._sum.value || 0;
 
-      // get expense
-      const expenseAgg = await prisma.financeItem.aggregate({
-        _sum: {
-          value: true,
-        },
-        where: {
-          type: "expense",
-        },
-      });
-      const expense = expenseAgg._sum.value || 0;
-
-      const budget = income - expense;
-      res.status(200).json({
-        message: "Get budget successfully",
-        body: budget,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while getting the budget",
-      });
-    }
-  },
-
-  // @desc    Get income
-  // @route   GET /api/finance/income
-  // @access  Private
-  getIncome: async (_: Request, res: Response) => {
-    try {
-      const result = await prisma.financeItem.findMany({
-        where: {
-          type: "income",
-        },
-        include: {
-          OtherItem: true,
-        },
-      });
-
-      const income = result;
-
-      res.status(200).json({
-        message: "Get income successfully",
-        body: income,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while getting the income",
-      });
-    }
-  },
-
-  // @desc    Get expense
-  // @route   GET /api/finance/expense
-  // @access  Private
-  getExpense: async (_: Request, res: Response) => {
-    try {
-      const expenses = await prisma.financeItem.findMany({
-        where: {
-          type: "expense",
-        },
-        include: {
-          OtherItem: true,
-        },
-      });
-
-      res.status(200).json({
-        message: "Get expense successfully",
-        body: expenses,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while getting the expense",
-      });
-    }
-  },
-
-  // @desc    Add a subscription
-  // @route   POST /api/finance/subscription
-  // @access  Private
-  addSubscription: async (req: Request, res: Response) => {
-    try {
-      const {
-        value,
-        sectorBaseName,
-        sectorSuffixName,
-        termNumber,
-        weekNumber,
-      } = req.body as {
-        value: number;
-        sectorBaseName: string;
-        sectorSuffixName: string;
-        termNumber: string;
-        weekNumber: string;
-      };
-
-      // let result = await db.query(
-      //   `SELECT "itemId" AS id
-      //           FROM "Subscription"
-      //           WHERE "sectorBaseName" = $1 AND "sectorSuffixName" = $2 AND "termNumber" = $3 AND "weekNumber" = $4;`,
-      //   [sectorBaseName, sectorSuffixName, termNumber, weekNumber],
-      // );
-
-      let subscr = await prisma.subscription.findFirst({
-        where: {
-          sectorBaseName: sectorBaseName,
-          sectorSuffixName: sectorSuffixName,
-          weekNumber: parseInt(weekNumber),
-          termNumber: parseInt(termNumber),
-        },
-      });
-
-      let result: any;
-
-      if (!subscr) {
-        result = await prisma.financeItem.create({
-          data: {
-            value: value,
-            type: "income",
-            timestamp: new Date(),
-          },
-        });
-      } else {
-        result = await prisma.financeItem.update({
-          where: {
-            itemId: subscr.itemId,
-          },
-          data: {
-            value: value,
-            timestamp: new Date(),
-          },
-        });
-      }
-
-      const financeItem = result;
-
-      result = await prisma.subscription.upsert({
-        where: {
-          itemId: financeItem.itemId,
-        },
-        update: {
-          sectorBaseName: sectorBaseName,
-          sectorSuffixName: sectorSuffixName,
-          weekNumber: parseInt(weekNumber),
-          termNumber: parseInt(termNumber),
-        },
-        create: {
-          itemId: financeItem.itemId,
-          sectorBaseName: sectorBaseName,
-          sectorSuffixName: sectorSuffixName,
-          weekNumber: parseInt(weekNumber),
-          termNumber: parseInt(termNumber),
-        },
-      });
-
-      const subscription = result;
-
-      // Notify general captains
-      const generalCaptains = await prisma.captain.findMany({
-        where: {
-          type: "general",
-        },
-      });
-
-      const notifications = generalCaptains.map((captain) => ({
-        captainId: captain.captainId,
-        type: "financeItemCreated",
-        status: "UNREAD",
-        title: "تحديث اشتراك",
-        message: `تم ${subscr ? "تحديث" : "إنشاء"} اشتراك لقطاع ${sectorBaseName} ${sectorSuffixName} بقيمة ${value}.`,
-      }));
-
-      const sentMsgs = await prisma.notification.createMany({
-        data: notifications as Notification[],
-      });
-
-      console.log(sentMsgs);
-
-      res.status(200).json({
-        message: "Add subscription successfully",
-        body: { financeItem: financeItem, subscription: subscription },
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while adding subscription",
-      });
-    }
-  },
-
-  // @desc    Get a subscription
-  // @route   GET /api/finance/subscription
-  // @access  Private
-  getSubscription: async (req: GetSubscriptionRequest, res: Response) => {
-    try {
-      let { sectorBaseName, sectorSuffixName, weekNumber, termNumber }: any =
-        req.query;
-
-      if (weekNumber === undefined)
-        weekNumber = req?.currentWeek?.weekNumber.toString();
-
-      if (termNumber === undefined)
-        termNumber = req?.currentWeek?.termNumber.toString();
-
-      const subscription = await prisma.subscription.findFirst({
-        where: {
-          sectorBaseName: sectorBaseName,
-          sectorSuffixName: sectorSuffixName,
-          weekNumber: parseInt(weekNumber),
-          termNumber: parseInt(termNumber),
-        },
-        include: {
-          FinanceItem: true,
-        },
-      });
-
-      res.status(200).json({
-        message: "Get subscription successfully",
-        body: subscription?.FinanceItem.value,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while getting the subscription",
-      });
-    }
-  },
-
-  // @desc    Get all subscriptions of current week
-  // @route   GET /api/finance/subscription/all
-  // @access  Private
-  getAllSubscriptionsOfCurrentWeek: async (req: any, res: Response) => {
-    try {
-      const subscription = await prisma.subscription.findMany({
-        where: {
-          termNumber: req.currentWeek.termNumber,
-          weekNumber: req.currentWeek.weekNumber,
-        },
-        include: {
-          FinanceItem: true,
-        },
-      });
-
-      const subscriptionValue = subscription.reduce(
-        (acc, curr) => acc + curr.FinanceItem.value,
-        0,
-      );
-
-      res.status(200).json({
-        message: "Get subscription successfully",
-        body: subscriptionValue,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while getting the subscription",
-      });
-    }
-  },
-
-  // @desc    Add an other item
-  // @route   POST /api/finance/otherItem
-  // @access  Private
-  addOtherItem: async (req: Request, res: Response) => {
-    try {
-      const { value, type, description } = req.body;
-
-      const financeItem = await prisma.financeItem.create({
-        data: {
-          value: parseInt(value),
-          type: type,
-          timestamp: new Date(),
-        },
-      });
-      const otherItem = await prisma.otherItem.create({
-        data: {
-          description: description,
-          itemId: financeItem.itemId,
-        },
-      });
-
-      res.status(200).json({
-        message: "Add other item successfully",
-        body: { financeItem: financeItem, otherItem: otherItem },
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while adding other item",
-      });
-    }
-  },
+  getBudget : asyncDec(getBudget),
+  getIncome : asyncDec(getIncome),
+  getExpense : asyncDec(getExpense),
+  addSubscription : asyncDec(addSubscription),
+  getAllSubscriptionsOfCurrentWeek : asyncDec(getAllSubscriptionsOfCurrentWeek),
+  getSubscription : asyncDec(getSubscription),
+  addOtherItem : asyncDec(addOtherItem),
 };
 
 export default financeController;
--- server/controllers/term.controller.ts
+++ server/controllers/term.controller.ts
@@ -1,253 +1,237 @@
 import { Response } from "express";
 import { prisma } from "../database/db";
+import AppError from "../utils/AppError";
+import asyncDec from "../utils/asyncDec";
 
 // ==============================================
 // TODO: needs integration testing
 // ==============================================
 
+
+// @desc    Get a term
+// @route   GET /api/term/
+// @access  Private
+
+async function getTerm(req: any, res: Response){
+
+  if(req.currentTerm.termNumber === 0){
+    throw new AppError(400, "There is no terms in the system", "لا توجد فترات في النظام");
+  }
+
+  res.status(200).json({
+    message: "Current term found successfully",
+    body: req.currentTerm
+  });
+
+}
+
+
+// @desc    Add a term
+// @route   POST /api/term/
+// @access  Private
+
+async function addTerm(req: any, res: Response){
+
+  const { termName, startDate, endDate } = req.body;
+
+  const currentDate = new Date();
+  const startDateObj = new Date(startDate);
+  const endDateObj = new Date(endDate);
+ 
+  if (startDateObj >= endDateObj || endDateObj < currentDate) {
+    throw new AppError(400, "Invalid dates", "تواريخ غير صالحة");
+  }
+
+  if (
+    req.currentTerm.termNumber &&
+    req.currentTerm.endDate &&
+    req.currentTerm.endDate >= startDateObj
+  ) {
+    throw new AppError(400, "Invalid start date: Overlapping terms", "تاريخ بدء غير صالح: تداخل الفصول");
+  }
+
+  const termNumber = req.currentTerm.termNumber + 1;
+
+  const newTerm = await prisma.term.create({
+    data: {
+      termNumber,
+      termName,
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
+    },
+  });
+
+  res.status(201).json({
+    message: "Term added successfully",
+    body: newTerm,
+  });
+
+}
+
+
+// @desc    Update a term
+// @route   PATCH /api/term/
+// @access  Private
+
+async function updateTerm(req: any, res: Response){
+
+  const { termName, startDate, endDate } = req.body;
+
+  const currentDate = new Date();
+  const startDateObj = new Date(startDate);
+  const endDateObj = new Date(endDate);
+
+  if (startDateObj >= endDateObj) {
+    throw new AppError(400, "Invalid dates", "تواريخ غير صالحة");
+  }
+
+  let prevTerm = await prisma.term.findMany({
+    orderBy: {
+      termNumber: "desc",
+    },
+    take: 1,
+    skip: 1,
+  });
+
+  if (!prevTerm.length) {
+    req.previousTerm = {
+      termNumber: 0,
+    };
+  } else req.previousTerm = prevTerm[0];
+
+  if (
+    req?.previousTerm?.termNumber &&
+    req?.previousTerm?.endDate &&
+    req?.previousTerm?.endDate >= startDateObj
+  ) {
+    throw new AppError(400, "Invalid start date: Overlapping terms", "تاريخ بدء غير صالح: تداخل الفصول");
+  }
+
+  const updatedTerm = await prisma.term.update({
+    where: {
+      termNumber: req.currentTerm.termNumber,
+    },
+    data: {
+      termName,
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
+    },
+  });
+
+  res.status(200).json({
+    message: "Term updated successfully",
+    body: updatedTerm,
+  });
+}
+
+// @desc    Get a week
+// @route   GET /api/term/week
+// @access  Private
+
+async function getCurrentWeek(req: any, res: Response){
+
+  if(req.currentWeek.weekNumber === 0){
+    throw new AppError(400, "There is no weeks or terms in the system", "لا توجد أسابيع أو فصول في النظام");
+  }
+
+  res.status(200).json({
+    message: "Current week found successfully",
+    body: req.currentWeek
+  });
+
+}
+
+
+// @desc    Get all weeks in term
+// @route   GET /api/term/week/all
+// @access  Private
+
+async function getAllWeeks(req: any, res: Response){
+
+  const weeks = await prisma.week.findMany({
+    where: {
+      termNumber: req.currentTerm.termNumber,
+    },
+  });
+
+  res.status(200).json({
+    message: "all weeks in current term found successfully",
+    body: weeks,
+  });
+}
+
+
+// @desc    Cancel a week
+// @route   PATCH /api/term/week
+// @access  Private
+
+async function cancelWeek(req: any, res: Response){
+
+  if (req.currentWeek.weekNumber === 0) {
+    throw new AppError(400, "There is no weeks or terms in the system to be cancelled", "لا توجد أسابيع أو فصول في النظام للإلغاء");
+  }
+
+  const termNumber = parseInt(req.currentWeek.termNumber);
+  const weekNumber = parseInt(req.currentWeek.weekNumber);
+
+  // TODO: needs testing
+  const updatedWeek = await prisma.week.update({
+    where: {
+      weekNumber_termNumber: {
+        termNumber,
+        weekNumber,
+      },
+    },
+    data: {
+      cancelled: true,
+    },
+  });
+
+  res.status(200).json({
+    message: "Week cancelled successfully",
+    body: updatedWeek,
+  });
+}
+
+
+
+
+// @desc    Get remaining weeks
+// @route   GET /api/term/remaining
+// @access  Private
+
+async function getRemainingWeeks(req: any, res: Response){
+
+  const currentDate = new Date();
+
+  if (
+    req.currentTerm.termNumber === 0 ||
+    (req.currentTerm.endDate && req.currentTerm.endDate < currentDate)
+  ) {
+    throw new AppError(400, "There is no running term in the system", "لا توجد فصول جارية في النظام");
+  }
+
+  const remainingWeeks = Math.ceil(
+    (req.currentTerm.endDate.getTime() - currentDate.getTime()) /
+      (1000 * 60 * 60 * 24 * 7),
+  );
+
+  res.status(200).json({
+    message: "Remaining weeks found successfully",
+    body: remainingWeeks,
+  });
+}
+
+
+
 const termController = {
-  // @desc    Get a term
-  // @route   GET /api/term/
-  // @access  Private
-  getTerm: async (req: any, res: Response) => {
-    if (req.currentTerm.termNumber === 0) {
-      return res.status(400).json({
-        error: "There is no terms in the system",
-      });
-    }
-    res.status(200).json({
-      message: "Current term found successfully",
-      body: req.currentTerm,
-    });
-  },
-
-  // @desc    Add a term
-  // @route   POST /api/term/
-  // @access  Private
-  addTerm: async (req: any, res: Response) => {
-    try {
-      const { termName, startDate, endDate } = req.body;
-
-      const currentDate = new Date();
-      const startDateObj = new Date(startDate);
-      const endDateObj = new Date(endDate);
-      if (startDateObj >= endDateObj || endDateObj < currentDate) {
-        return res.status(400).json({
-          error: "Invalid dates",
-        });
-      }
-      if (
-        req.currentTerm.termNumber &&
-        req.currentTerm.endDate &&
-        req.currentTerm.endDate >= startDateObj
-      ) {
-        return res.status(400).json({
-          error: "Invalid start date: Overlapping terms",
-        });
-      }
-
-      const termNumber = req.currentTerm.termNumber + 1;
-
-      const newTerm = await prisma.term.create({
-        data: {
-          termNumber,
-          termName,
-          startDate: new Date(startDate),
-          endDate: new Date(endDate),
-        },
-      });
-
-      res.status(201).json({
-        message: "Term added successfully",
-        body: newTerm,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while adding the term",
-      });
-    }
-  },
-
-  // @desc    Update a term
-  // @route   PATCH /api/term/
-  // @access  Private
-  updateTerm: async (req: any, res: Response) => {
-    try {
-      const { termName, startDate, endDate } = req.body;
-
-      const currentDate = new Date();
-      const startDateObj = new Date(startDate);
-      const endDateObj = new Date(endDate);
-
-      if (startDateObj >= endDateObj) {
-        return res.status(400).json({
-          error: "Invalid dates",
-        });
-      }
-
-      let prevTerm = await prisma.term.findMany({
-        orderBy: {
-          termNumber: "desc",
-        },
-        take: 1,
-        skip: 1,
-      });
-
-      if (!prevTerm.length) {
-        req.previousTerm = {
-          termNumber: 0,
-        };
-      } else req.previousTerm = prevTerm[0];
-
-      if (
-        req?.previousTerm?.termNumber &&
-        req?.previousTerm?.endDate &&
-        req?.previousTerm?.endDate >= startDateObj
-      ) {
-        return res.status(400).json({
-          error: "Invalid start date: Overlapping terms",
-        });
-      }
-
-      const updatedTerm = await prisma.term.update({
-        where: {
-          termNumber: req.currentTerm.termNumber,
-        },
-        data: {
-          termName,
-          startDate: new Date(startDate),
-          endDate: new Date(endDate),
-        },
-      });
-
-      res.status(200).json({
-        message: "Term updated successfully",
-        body: updatedTerm,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while updating the term",
-      });
-    }
-  },
-
-  // @desc    Get a week
-  // @route   GET /api/term/week
-  // @access  Private
-  getCurrentWeek: async (req: any, res: Response) => {
-    try {
-      if (req.currentWeek.weekNumber === 0) {
-        return res.status(400).json({
-          error: "There is no weeks or terms in the system",
-        });
-      }
-      res.status(200).json({
-        message: "Current week found successfully",
-        body: req.currentWeek,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while getting the week",
-      });
-    }
-  },
-
-  // @desc    Get all weeks in term
-  // @route   GET /api/term/week/all
-  // @access  Private
-  getAllWeeks: async (req: any, res: Response) => {
-    try {
-      const weeks = await prisma.week.findMany({
-        where: {
-          termNumber: req.currentTerm.termNumber,
-        },
-      });
-
-      res.status(200).json({
-        message: "all weeks in current term found successfully",
-        body: weeks,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while getting all weeks",
-      });
-    }
-  },
-
-  // @desc    Cancel a week
-  // @route   PATCH /api/term/week
-  // @access  Private
-  cancelWeek: async (req: any, res: Response) => {
-    try {
-      if (req.currentWeek.weekNumber === 0) {
-        return res.status(400).json({
-          error: "There is no weeks or terms in the system to be cancelled",
-        });
-      }
-
-      const termNumber = parseInt(req.currentWeek.termNumber);
-      const weekNumber = parseInt(req.currentWeek.weekNumber);
-
-      // TODO: needs testing
-      const updatedWeek = await prisma.week.update({
-        where: {
-          weekNumber_termNumber: {
-            termNumber,
-            weekNumber,
-          },
-        },
-        data: {
-          cancelled: true,
-        },
-      });
-
-      res.status(200).json({
-        message: "Week cancelled successfully",
-        body: updatedWeek,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while cancelling the week",
-      });
-    }
-  },
-
-  // @desc    Get remaining weeks
-  // @route   GET /api/term/remaining
-  // @access  Private
-  getRemainingWeeks: async (req: any, res: Response) => {
-    try {
-      const currentDate = new Date();
-      if (
-        req.currentTerm.termNumber === 0 ||
-        (req.currentTerm.endDate && req.currentTerm.endDate < currentDate)
-      ) {
-        return res.status(400).json({
-          error: "There is no running term in the system",
-        });
-      }
-
-      const remainingWeeks = Math.ceil(
-        (req.currentTerm.endDate.getTime() - currentDate.getTime()) /
-          (1000 * 60 * 60 * 24 * 7),
-      );
-
-      res.status(200).json({
-        message: "Remaining weeks found successfully",
-        body: remainingWeeks,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        error: "An error occurred while getting the remaining weeks",
-      });
-    }
-  },
+  getTerm: asyncDec(getTerm),
+  addTerm: asyncDec(addTerm),
+  updateTerm: asyncDec(updateTerm),
+  getCurrentWeek: asyncDec(getCurrentWeek),
+  getAllWeeks: asyncDec(getAllWeeks),
+  cancelWeek: asyncDec(cancelWeek),
+  getRemainingWeeks: asyncDec(getRemainingWeeks)
 };
 
 export default termController;
