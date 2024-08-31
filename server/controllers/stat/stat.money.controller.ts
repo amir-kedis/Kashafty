248317bb2958da0864f8e352bdb1428932f0d1c2
shehab299 <shehab299@outlook.com>
2024-08-31 15:11:39 +0300

    refactored stats

--- server/controllers/stat/stat.money.controller.ts
+++ server/controllers/stat/stat.money.controller.ts
@@ -1,4 +1,4 @@
-/*============================================================================
+/*==================================================================================================
  *
  *
  *    ███╗   ███╗ ██████╗ ███╗   ██╗███████╗██╗   ██╗
@@ -12,336 +12,274 @@
  *
  *    Author: Amir Kedis
  *
- *==========================================================================*/
+ *==================================================================================================*/
 
 import { Request, Response } from "express";
 import { prisma } from "../../database/db";
+import AppError from "../../utils/AppError";
+import asyncDec from "../../utils/asyncDec";
 
-const statMoneyController = {
-  /* getTotalMoney
-   *
-   * @desc gets the total money in the bank
-   * @endpoint GET /api/stat/money/total
-   */
-  getTotalMoney: async (_req: Request, res: Response) => {
-    try {
+async function getTotalMoney(req: Request, res: Response) {
+  const income = await prisma.financeItem.aggregate({
+    _sum: {
+      value: true,
+    },
+    where: {
+      type: "income",
+    },
+  });
+
+  const expense = await prisma.financeItem.aggregate({
+    _sum: {
+      value: true,
+    },
+    where: {
+      type: "expense",
+    },
+  });
+
+  return res.status(200).json({
+    message: "retrived money successfully.",
+    totalMoney: (income._sum.value ?? 0) - (expense._sum.value ?? 0),
+  });
+}
+
+async function getTotalIncome(req: Request, res: Response) {
+  const income = await prisma.financeItem.aggregate({
+    _sum: {
+      value: true,
+    },
+    where: {
+      type: "income",
+    },
+  });
+
+  return res.status(200).json({
+    message: "retrived money successfully.",
+    totalMoney: income._sum.value ?? 0,
+  });
+}
+
+async function getTotalExpense(req: Request, res: Response) {
+  const expense = await prisma.financeItem.aggregate({
+    _sum: {
+      value: true,
+    },
+    where: {
+      type: "expense",
+    },
+  });
+
+  return res.status(200).json({
+    message: "retrived money successfully.",
+    totalMoney: expense._sum.value ?? 0,
+  });
+}
+
+async function getCurrentWeekSubscription(req: Request, res: Response) {
+  const today = new Date();
+
+  const currentWeek = await prisma.week.findFirst({
+    where: {
+      startDate: {
+        gte: new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000),
+      },
+    },
+  });
+
+  if (!currentWeek) {
+    throw new AppError(404, "Current week not found", "الأسبوع الحالي غير موجود");
+  }
+
+  const subscriptions = await prisma.subscription.findMany({
+    where: {
+      weekNumber: currentWeek.weekNumber,
+      termNumber: currentWeek.termNumber,
+    },
+    include: {
+      FinanceItem: true,
+    },
+  });
+
+  const totalSub = subscriptions.reduce((total, subscription) => {
+    return total + (subscription.FinanceItem?.value || 0);
+  }, 0);
+
+  return res.status(200).json({
+    message: "current week subscription retrieved successfully.",
+    totalMoney: totalSub,
+  });
+}
+
+async function getMoneyLineChart(req: Request, res: Response) {
+  const currentTerm = await prisma.term.findFirst({
+    orderBy: {
+      termNumber: "desc",
+    },
+    include: {
+      Week: true,
+    },
+  });
+
+  if (!currentTerm) {
+    throw new AppError(404, "Current term not found", "الفصل الدراسي الحالي غير موجود");
+  }
+
+  const weeklyTotals = await Promise.all(
+    currentTerm.Week.map(async (week) => {
       const income = await prisma.financeItem.aggregate({
         _sum: {
           value: true,
         },
         where: {
           type: "income",
-        },
-      });
-
-      const expense = await prisma.financeItem.aggregate({
-        _sum: {
-          value: true,
-        },
-        where: {
-          type: "expense",
-        },
-      });
-
-      res.status(200).json({
-        message: "retrived money successfully.",
-        totalMoney: (income._sum.value ?? 0) - (expense._sum.value ?? 0),
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  },
-
-  /* getTotalIncome
-   *
-   * @desc gets the total income
-   * @endpoint GET /api/stat/money/income
-   */
-  getTotalIncome: async (_req: Request, res: Response) => {
-    try {
-      const income = await prisma.financeItem.aggregate({
-        _sum: {
-          value: true,
-        },
-        where: {
-          type: "income",
-        },
-      });
-
-      res.status(200).json({
-        message: "retrived money successfully.",
-        totalMoney: income._sum.value ?? 0,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  },
-
-  /* getTotalExpense
-   *
-   * @desc gets the total expense
-   * @endpoint GET /api/stat/money/spent
-   */
-  getTotalExpense: async (_req: Request, res: Response) => {
-    try {
-      const expense = await prisma.financeItem.aggregate({
-        _sum: {
-          value: true,
-        },
-        where: {
-          type: "expense",
-        },
-      });
-
-      res.status(200).json({
-        message: "retrived money successfully.",
-        totalMoney: expense._sum.value ?? 0,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  },
-
-  /* getCurrentWeekSubscription
-   *
-   * @desc gets the current week subscription
-   * @endpoint GET /api/stat/money/current-week-subscription
-   */
-  getCurrentWeekSubscription: async (_req: Request, res: Response) => {
-    try {
-      const today = new Date();
-
-      const currentWeek = await prisma.week.findFirst({
-        where: {
-          startDate: {
-            gte: new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000),
+          timestamp: {
+            lt: new Date(
+              week.startDate.getTime() + 7 * 24 * 60 * 60 * 1000,
+            ),
           },
         },
       });
 
+      const expense = await prisma.financeItem.aggregate({
+        _sum: {
+          value: true,
+        },
+        where: {
+          type: "expense",
+          timestamp: {
+            lt: new Date(
+              week.startDate.getTime() + 7 * 24 * 60 * 60 * 1000,
+            ),
+          },
+        },
+      });
+
+      return {
+        weekNumber: week.weekNumber,
+        totalMoney: (income._sum.value ?? 0) - (expense._sum.value ?? 0),
+      };
+    }),
+  );
+
+  return res.status(200).json({
+    message: "Data retrieved successfully.",
+    weeklyTotals: weeklyTotals,
+  });
+}
+
+async function getIncomeExpenseStackedChart(req: Request, res: Response) {
+  const currentTerm = await prisma.term.findFirst({
+    orderBy: {
+      termNumber: "desc",
+    },
+    include: {
+      Week: true,
+    },
+  });
+
+  if (!currentTerm) {
+    throw new AppError(404, "Current term not found", "الفصل الدراسي الحالي غير موجود");
+  }
+
+  const weeklyData = await Promise.all(
+    currentTerm.Week.map(async (week) => {
+      const income = await prisma.financeItem.aggregate({
+        _sum: {
+          value: true,
+        },
+        where: {
+          type: "income",
+          timestamp: {
+            lt: new Date(
+              week.startDate.getTime() + 7 * 24 * 60 * 60 * 1000,
+            ),
+          },
+        },
+      });
+
+      const expense = await prisma.financeItem.aggregate({
+        _sum: {
+          value: true,
+        },
+        where: {
+          type: "expense",
+          timestamp: {
+            lt: new Date(
+              week.startDate.getTime() + 7 * 24 * 60 * 60 * 1000,
+            ),
+          },
+        },
+      });
+
+      return {
+        weekNumber: week.weekNumber,
+        totalIncome: income._sum.value ?? 0,
+        totalExpense: expense._sum.value ?? 0,
+      };
+    }),
+  );
+
+  return res.status(200).json({
+    message: "Data retrieved successfully.",
+    weeklyData: weeklyData,
+  });
+}
+
+async function getSubscriptionLineChart(req: Request, res: Response) {
+  const currentTerm = await prisma.term.findFirst({
+    orderBy: {
+      termNumber: "desc",
+    },
+    include: {
+      Week: true,
+    },
+  });
+
+  if (!currentTerm) {
+    throw new AppError(404, "Current term not found", "الفصل الدراسي الحالي غير موجود");
+  }
+
+  const weeklyData = await Promise.all(
+    currentTerm.Week.map(async (week) => {
       const subscriptions = await prisma.subscription.findMany({
         where: {
-          weekNumber: currentWeek?.weekNumber,
-          termNumber: currentWeek?.termNumber,
+          weekNumber: week.weekNumber,
+          termNumber: week.termNumber,
         },
         include: {
           FinanceItem: true,
         },
       });
 
-      const totalSub = subscriptions.reduce((total, subscription) => {
-        return total + (subscription.FinanceItem?.value || 0);
-      }, 0);
-
-      res.status(200).json({
-        message: "current week subscription retrived succfully.",
-        totalMoney: totalSub,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  },
-
-  /* getCurrentWeekSubscription
-   *
-   * @desc  get the total money at each week to form a line chart
-   * @endpoint GET /api/stat/money/line-chart
-   */
-  getMoneyLineChart: async (_req: Request, res: Response) => {
-    try {
-      const currentTerm = await prisma.term.findFirst({
-        orderBy: {
-          termNumber: "desc",
+      const totalSubscription = subscriptions.reduce(
+        (total, subscription) => {
+          return total + (subscription.FinanceItem?.value || 0);
         },
-        include: {
-          Week: true,
-        },
-      });
-
-      if (!currentTerm) {
-        return res
-          .status(404)
-          .json({ message: "لم يتم العثور على الفصل الحالي" });
-      }
-
-      const weeklyTotals = await Promise.all(
-        currentTerm.Week.map(async (week) => {
-          const income = await prisma.financeItem.aggregate({
-            _sum: {
-              value: true,
-            },
-            where: {
-              type: "income",
-              timestamp: {
-                lt: new Date(
-                  week.startDate.getTime() + 7 * 24 * 60 * 60 * 1000,
-                ),
-              },
-            },
-          });
-
-          const expense = await prisma.financeItem.aggregate({
-            _sum: {
-              value: true,
-            },
-            where: {
-              type: "expense",
-              timestamp: {
-                lt: new Date(
-                  week.startDate.getTime() + 7 * 24 * 60 * 60 * 1000,
-                ),
-              },
-            },
-          });
-
-          return {
-            weekNumber: week.weekNumber,
-            totalMoney: (income._sum.value ?? 0) - (expense._sum.value ?? 0),
-          };
-        }),
+        0,
       );
 
-      res.status(200).json({
-        message: "تم الحصول على البيانات بنجاح",
-        weeklyTotals: weeklyTotals,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  },
+      return {
+        weekNumber: week.weekNumber,
+        totalSubscription: totalSubscription,
+      };
+    }),
+  );
 
-  /* getIncomeAndExpenseStackedLineChart
-   *
-   * @desc  get the total income and total expense at each week to form a stacked line chart
-   * @endpoint GET /api/stat/money/income-expense-chart
-   */
-  getIncomeExpenseStackedChart: async (_req: Request, res: Response) => {
-    try {
-      const currentTerm = await prisma.term.findFirst({
-        orderBy: {
-          termNumber: "desc",
-        },
-        include: {
-          Week: true,
-        },
-      });
+  return res.status(200).json({
+    message: "Data retrieved successfully.",
+    weeklyData: weeklyData,
+  });
+}
 
-      if (!currentTerm) {
-        return res
-          .status(404)
-          .json({ message: "لم يتم العثور على الفصل الحالي" });
-      }
-
-      const weeklyData = await Promise.all(
-        currentTerm.Week.map(async (week) => {
-          const income = await prisma.financeItem.aggregate({
-            _sum: {
-              value: true,
-            },
-            where: {
-              type: "income",
-              timestamp: {
-                lt: new Date(
-                  week.startDate.getTime() + 7 * 24 * 60 * 60 * 1000,
-                ),
-              },
-            },
-          });
-
-          const expense = await prisma.financeItem.aggregate({
-            _sum: {
-              value: true,
-            },
-            where: {
-              type: "expense",
-              timestamp: {
-                lt: new Date(
-                  week.startDate.getTime() + 7 * 24 * 60 * 60 * 1000,
-                ),
-              },
-            },
-          });
-
-          return {
-            weekNumber: week.weekNumber,
-            totalIncome: income._sum.value ?? 0,
-            totalExpense: expense._sum.value ?? 0,
-          };
-        }),
-      );
-
-      res.status(200).json({
-        message: "تم الحصول على البيانات بنجاح",
-        weeklyData: weeklyData,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  },
-
-  /* getSubscriptionLineChart
-   *
-   * @desc  get the total subscription of each week
-   * @endpoint GET /api/stat/money/subscription-line-chart
-   */
-  getSubscriptionLineChart: async (_req: Request, res: Response) => {
-    try {
-      const currentTerm = await prisma.term.findFirst({
-        orderBy: {
-          termNumber: "desc",
-        },
-        include: {
-          Week: true,
-        },
-      });
-
-      if (!currentTerm) {
-        return res
-          .status(404)
-          .json({ message: "لم يتم العثور على الفصل الحالي" });
-      }
-
-      const weeklyData = await Promise.all(
-        currentTerm.Week.map(async (week) => {
-          const subscriptions = await prisma.subscription.findMany({
-            where: {
-              weekNumber: week.weekNumber,
-              termNumber: week.termNumber,
-            },
-            include: {
-              FinanceItem: true,
-            },
-          });
-
-          const totalSubscription = subscriptions.reduce(
-            (total, subscription) => {
-              return total + (subscription.FinanceItem?.value || 0);
-            },
-            0,
-          );
-
-          return {
-            weekNumber: week.weekNumber,
-            totalSubscription: totalSubscription,
-          };
-        }),
-      );
-
-      res.status(200).json({
-        message: "تم الحصول على البيانات بنجاح",
-        weeklyData: weeklyData,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  },
+const statMoneyController = {
+  getTotalMoney: asyncDec(getTotalMoney),
+  getTotalIncome: asyncDec(getTotalIncome),
+  getTotalExpense: asyncDec(getTotalExpense),
+  getCurrentWeekSubscription: asyncDec(getCurrentWeekSubscription),
+  getMoneyLineChart: asyncDec(getMoneyLineChart),
+  getIncomeExpenseStackedChart: asyncDec(getIncomeExpenseStackedChart),
+  getSubscriptionLineChart: asyncDec(getSubscriptionLineChart),
 };
 
 export default statMoneyController;
