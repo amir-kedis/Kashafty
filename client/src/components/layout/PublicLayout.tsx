a475c4bc5aefc12f1de91ae2f13ea6fb74fbd4b6
Asmaa-204 <130288326+Asmaa-204@users.noreply.github.com>
2024-09-05 14:45:24 +0300

    fix: scroll to top when navigating to pages (#71)

--- client/src/components/layout/PublicLayout.tsx
+++ client/src/components/layout/PublicLayout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Nav from "../common/nav";
 import Footer from "../common/Footer";
+import { useEffect } from "react";
 
 export default function PublicLayout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
   return (
     <div className="public_layout">
       <Nav />
