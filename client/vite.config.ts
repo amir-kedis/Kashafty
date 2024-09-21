014c838388b2f5b5fb2287fed3f3c43f596c6cb7
Amir Kedis <88613195+amir-kedis@users.noreply.github.com>
2024-09-21 22:13:34 +0300

    devops: enhance docker developer experience and setup coolify home server production setup (#86)
    * feat: add better development experince with docker
    
    * feat: add production ready docker compose
    
    * docs: documenting dev setup with dockercompose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  process.env = { ...process.env, ...env };
+
+  const port = parseInt(env.VITE_PORT) || 3000;
+
+  console.log("Vite development server is running at port", port);
+  console.log("backend url is", env.VITE_API_URL);
+
+  return {
+    plugins: [react()],
+    server: {
+      port: port,
+    },
+    define: {
+      "import.meta.env": JSON.stringify(env),
+    },
+  };
 });
